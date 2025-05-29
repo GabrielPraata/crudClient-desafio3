91cbf715171683dcb31771dbc59d782bc8c74d36
Gabriel Prata <ghprata7245@gmail.com>
2025-05-29 08:41:39 -0300

    Configuracao inicial

---
+++ src/main/java/com/Prata/crudClient_desafio3/CrudClientDesafio3Application.java
@@ -0,0 +1,13 @@
+package com.Prata.crudClient_desafio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientDesafio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientDesafio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Prata/crudClient_desafio3/CrudClientDesafio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prata.crudClient_desafio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientDesafio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

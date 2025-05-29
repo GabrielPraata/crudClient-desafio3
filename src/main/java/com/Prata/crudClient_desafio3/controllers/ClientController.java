ff81b20e4511cdd61655d2302ca8081ebbbe8721
Gabriel Prata <ghprata7245@gmail.com>
2025-05-29 09:32:42 -0300

    Adicionado Controller, seed database e GET products Id e GET products paged

---
+++ src/main/java/com/Prata/crudClient_desafio3/controllers/ClientController.java
@@ -0,0 +1,33 @@
+package com.Prata.crudClient_desafio3.controllers;
+
+import com.Prata.crudClient_desafio3.dto.ClientDTO;
+import com.Prata.crudClient_desafio3.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = service.fidAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+}
--- src/main/java/com/Prata/crudClient_desafio3/services/ClientService.java
+++ src/main/java/com/Prata/crudClient_desafio3/services/ClientService.java
@@ -1,7 +1,32 @@
 package com.Prata.crudClient_desafio3.services;
 
+import com.Prata.crudClient_desafio3.dto.ClientDTO;
+import com.Prata.crudClient_desafio3.entities.Client;
+import com.Prata.crudClient_desafio3.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> fidAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
+
 }

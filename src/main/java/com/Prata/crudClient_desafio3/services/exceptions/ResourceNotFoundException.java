1ed528ed9ab12851519fac3678fe40dc78644a42
Gabriel Prata <ghprata7245@gmail.com>
2025-05-29 11:06:37 -0300

    Exception handling

---
+++ src/main/java/com/Prata/crudClient_desafio3/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Prata.crudClient_desafio3.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
